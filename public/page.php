0aa87a3859a69a5597409fca06062b52192e38ff
Michael Slone <m.slone@gmail.com>
2017-05-23 11:59:44 +0000

    Classify most formats as imagelike or booklike.

--- public/page.php
+++ public/page.php
@@ -29,6 +29,8 @@ foreach ($doc as $key => $value) {
 }
 
 switch ($format) {
+case 'drawings (visual works)':
+    /* fall through */
 case 'images':
     $data['item'] = $templates['item-image']($flat);
     $data['scripts'] = $templates['script-image'](array_merge(
@@ -41,7 +43,37 @@ case 'images':
     ));
     print $templates['show']($data);
     break;
+case 'annual reports':
+    /* fall through */
+case 'architectural drawings (visual works)':
+    /* fall through */
+case 'archival material':
+    /* fall through */
+case 'athletic publications':
+    /* fall through */
+case 'booklets':
+    /* fall through */
 case 'books':
+    /* fall through */
+case 'course catalogs':
+    /* fall through */
+case 'directories':
+    /* fall through */
+case 'indexes (reference sources)':
+    /* fall through */
+case 'journals':
+    /* fall through */
+case 'ledgers':
+    /* fall through */
+case 'maps':
+    /* fall through */
+case 'minutes':
+    /* fall through */
+case 'newsletters':
+    /* fall through */
+case 'newspapers':
+    /* fall through */
+case 'yearbooks':
     $pages = get_pages($id);
     if ($pages) {
         $data['scripts'] = $templates['script-book'](array(
